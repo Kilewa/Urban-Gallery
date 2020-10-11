937c27d352c7923c43509d543f59cb88b1a18abf
kilewa <kilewageorge230@gmail.com>
2020-10-11 23:29:53 +0300

    uploaded sample imgs & styles

--- gallery/views.py
+++ gallery/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from .models import Image, Location, Category
 
 
+
+
 def index(request):
     images = Image.get_all_images()
     locations = Location.objects.all()

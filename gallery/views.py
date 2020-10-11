29e06b8bec2243b00f254960485d903cd10f1a04
kilewa <kilewageorge230@gmail.com>
2020-10-11 10:25:49 +0300

    view for index

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http  import HttpResponse, Http404
+from django.shortcuts import render, redirect
+from .models import Image, Location, Category
 
-# Create your views here.
+
+def index(request):
+    images = Image.get_all_images()
+    locations = Location.objects.all()
+    title = 'Urban Gallery'
+
+    return render(request, 'index.html', {'title':title, 'images':images, 'locations':locations})
